716df1cc7c6bbfca1f8e5c96ad2e3a43abd9b822
harryscholes <harryscholes@gmail.com>
2019-03-28 17:33:18 +0000

    Initial sketch of ideas

--- README.md
+++ README.md
@@ -2,3 +2,26 @@
 
 [![Build Status](https://travis-ci.com/harryscholes/SaveFigs.jl.svg?branch=master)](https://travis-ci.com/harryscholes/SaveFigs.jl)
 [![Codecov](https://codecov.io/gh/harryscholes/SaveFigs.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/harryscholes/SaveFigs.jl)
+
+Save Plots.jl figures easily in multiple formats and sizes.
+
+# Installation
+
+```julia
+] add https://github.com/harryscholes/SaveFigs.jl
+```
+
+## Examples
+
+```julia
+julia> using Plots, SaveFigs
+
+julia> fig = plot(rand(10))
+
+julia> savefigs(fig, "fig1", "pdf", "png")
+
+julia> plot(rand(100))
+
+julia> savefigs("fig2", :pdf, :png; sizes=[(300,300), (600,600)])
+
+```
--- src/SaveFigs.jl
+++ src/SaveFigs.jl
@@ -1,5 +1,38 @@
 module SaveFigs
 
-greet() = print("Hello World!")
+export savefigs
 
-end # module
+using Plots
+
+function savefigs(fig, fname::AbstractString, formats...; sizes=nothing)
+    isdir(fname) || mkdir(fname)
+    if isnothing(sizes)
+        dir = fname
+        _saveformats(fig, dir, fname, formats)
+    else
+        dir = pwd()
+        _saveformats_sizes(fig, dir, fname, formats, sizes)
+    end
+end
+
+function savefigs(fname::AbstractString, formats...; sizes=nothing)
+    savemultifigs(Plots.current(), fname, formats...; sizes=sizes)
+end
+
+function _saveformats(fig, dir, fname, formats)
+    for f = formats
+        savefig(fig, joinpath(dir, "$(fname).$(f)"))
+    end
+end
+
+function _saveformats_sizes(fig, dir, fname, formats, sizes)
+    # Save original size
+    _saveformats(fig, fname, fname, formats)
+    # Resize and save
+    for (w,h) = sizes
+        plot!(fig, size=(w,h))
+        _saveformats(fig, fname, "$(fname)_$(w)x$(h)", formats)
+    end
+end
+
+end
